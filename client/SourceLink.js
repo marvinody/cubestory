9809945a38216a17dd4298118e84972fc7cb9bce
Manuel Bugallo <marvinody@gmail.com>
2019-11-18 19:19:04 -0500

    feat: adds source to pages

---
+++ client/SourceLink.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SourceLink() {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '0px',
+        right: '10px'
+      }}
+    >
+      <a href="https://github.com/marvinody/cubestory">source</a>
+    </div>
+  )
+}
--- client/app.js
+++ client/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Routes from './routes'
+import SourceLink from './SourceLink'
 
 const App = () => {
   return (
     <div>
       <Routes />
+      <SourceLink />
     </div>
   )
 }
